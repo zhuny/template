270141e1e3a677909ee8b6e45c8df5998a7dfa7a
zhuny <zhuny@kaist.ac.kr>
2020-04-12 22:14:55 +0900

    test code

--- tests/test_index.py
+++ tests/test_index.py
@@ -7,3 +7,7 @@ class IndexTest(unittest.TestCase):
     def setUp(self):
         self.client = app.test_client()
 
+    def test_get_restful(self):
+        resp1 = self.client.get("/index")
+        resp2 = self.client.get("/index/123")
+
